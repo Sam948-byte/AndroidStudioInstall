9ee7a54b3fbd8894be8384ea7eadfff775475846
Sam <techguy763@gmail.com>
2022-10-14 17:03:26 +0000

    add separate scripts for install and cloning plus delete self after done

--- install.sh
+++ install.sh
@@ -10,17 +10,10 @@ tar -xf android-studio-2021.3.1.17-linux.tar.gz
 
 rm android-studio-2021.3.1.17-linux.tar.gz
 
-mkdir gitClones
-
-cd gitClones
-
-git clone https://github.com/javajokers/2022.git
-git clone https://github.com/javajokers/code.git
-
-cd ~
-
 cd android-studio
 
 cd bin
+
+rm -rf ~/install
 
 ./studio.sh
