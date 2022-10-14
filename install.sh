f4b07fc73d890c76cc6739358ac70f621d3e1dfa
Sam <techguy763@gmail.com>
2022-10-14 17:33:46 +0000

    verbose tar

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ cd ~
 
 wget https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2021.3.1.17/android-studio-2021.3.1.17-linux.tar.gz
 
-tar -xf android-studio-2021.3.1.17-linux.tar.gz
+tar -xfv android-studio-2021.3.1.17-linux.tar.gz
 
 rm android-studio-2021.3.1.17-linux.tar.gz
 
