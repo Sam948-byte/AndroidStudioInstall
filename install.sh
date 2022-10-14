c0bb3f0b21eea8e2526a420e3d157463688d84d3
Sam <techguy763@gmail.com>
2022-10-14 16:01:40 +0000

    unzip

--- install.sh
+++ install.sh
@@ -5,3 +5,7 @@ sudo apt-get -y install default-jre
 cd ~
 
 wget https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2021.3.1.17/android-studio-2021.3.1.17-linux.tar.gz
+
+tar -xf android-studio-2021.3.1.17-linux.tar.gz
+
+rm android-studio-2021.3.1.17-linux.tar.gz
