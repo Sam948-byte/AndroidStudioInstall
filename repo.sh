077b12065c90c0a34de8a80c50e57df0e20565ef
Sam <techguy763@gmail.com>
2022-10-14 19:56:53 +0000

    add git repo

--- repo.sh
+++ repo.sh
@@ -6,6 +6,7 @@ mkdir gitClones
 
 cd gitClones
 
+git clone https://github.com/javajokers/FTCRobotController.git
 git clone https://github.com/javajokers/2022.git
 git clone https://github.com/javajokers/code.git
 
