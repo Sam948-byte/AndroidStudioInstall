e13568fd347350ec3b18d005297265c05a695246
Sam <techguy763@gmail.com>
2022-10-14 16:03:13 +0000

    that should do it

--- install.sh
+++ install.sh
@@ -9,3 +9,18 @@ wget https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2021.3.1.17/andr
 tar -xf android-studio-2021.3.1.17-linux.tar.gz
 
 rm android-studio-2021.3.1.17-linux.tar.gz
+
+mkdir gitClones
+
+cd gitClones
+
+git clone https://github.com/javajokers/2022.git
+git clone https://github.com/javajokers/code.git
+
+cd ~
+
+cd android-studio
+
+cd bin
+
+./studio.sh
