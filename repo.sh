c3ac7fc1543e349d9d0592db1b0e5b01d5984748
Sam <techguy763@gmail.com>
2022-10-18 14:29:56 +0000

    add git clone

--- repo.sh
+++ repo.sh
@@ -9,6 +9,7 @@ cd gitClones
 git clone https://github.com/javajokers/OldCodeUpdated.git
 git clone https://github.com/javajokers/2022.git
 git clone https://github.com/javajokers/code.git
+git clone https://github.com/Sam948-byte/FtcRobotController.git
 
 cd ../install
 
