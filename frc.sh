19934229fbd7074366912be24fb1019561ee7cea
Sam Freund <techguy763@gmail.com>
2023-05-01 11:45:16 -0500

    update version

--- frc.sh
+++ frc.sh
@@ -5,13 +5,13 @@ sudo apt-get -y install default-jre
 
 cd ~
 
-wget https://github.com/wpilibsuite/allwpilib/releases/download/v2022.4.1/WPILib_Linux-2022.4.1.tar.gz
+wget https://github.com/wpilibsuite/allwpilib/releases/download/v2022.4.1/WPILib_Linux-2022.4.3.tar.gz
 
-tar -xvf WPILib_Linux-2022.4.1.tar.gz
+tar -xvf WPILib_Linux-2022.4.3.tar.gz
 
-rm WPILib_Linux-2022.4.1.tar.gz
+rm WPILib_Linux-2022.4.3.tar.gz
 
-cd WPILib_Linux-2022.4.1
+cd WPILib_Linux-2022.4.3
 
 chmod +x WPILibInstaller
 
